a08a148f23cd3f1b5032a74d1bfd206bbf8ebdf5
matt.trice <matt.trice@toro.com>
2024-01-19 12:28:33 -0700

    refactor and run from python

--- flask-app/app.py
+++ flask-app/app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/ping/<ping>")
 def hello_world(ping):
     return f"{escape(ping)}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
--- test/ping-test.py
+++ test/ping-test.py
@@ -6,11 +6,15 @@ def test_ping():
     BASE_URL = (
         os.environ["BASE_URL"] if "BASE_URL" in os.environ else "http://localhost:5000"
     )
-    print(f"BASE_URL={BASE_URL}")
 
     url = f"{BASE_URL}/ping/"
     param = "horsetooth"
+
+    print(f"url={url + param}")
+
     response = requests.get(url + param)
+
     print(response.content)
+
     assert response.status_code == 200
     assert response.content == param.encode("utf-8")
